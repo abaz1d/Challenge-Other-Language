c337f313f067bf7dfb090480bb5dcee9d3a4f670
Abdul Aziz <abaz.id@192.168.1.15>
2023-07-30 09:08:04 +0700

    fix it

--- CH01/c1.js
+++ CH01/c1.js
@@ -1,14 +1,26 @@
 //CH1
 //==============================================================
-function sum(arr) {
-  var s = 0;
-  for (var i = 0; i < arr.length; i++) {
-    s = s + arr[i];
+// function sum(arr) {
+//   var s = 0;
+//   for (var i = 0; i < arr.length; i++) {
+//     s = s + arr[i];
+//   }
+//   console.log(s);
+// }
+
+// sum([1, 2, 7]);
+// sum([1, 4]);
+// sum([11]);
+// sum([10, 3, 6, 7, 9]);
+function sum(...numbers) {
+  let total = 0;
+  for (let number of numbers) {
+    total += number;
   }
-  console.log(s);
+  console.log(total);
 }
 
-sum([1, 2, 7]);
-sum([1, 4]);
-sum([11]);
-sum([10, 3, 6, 7, 9]);
+sum(1, 2, 7);
+sum(1, 4);
+sum(11);
+sum(10, 3, 6, 7, 9);
