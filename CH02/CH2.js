3f9c636be188c74da7d2fe3788a7c99aec8f5941
abaz1d <abaz.my.id@gmail.com>
2023-08-27 14:46:42 +0700

    use php lang

--- CH02/CH2.js
+++ CH02/CH2.js
@@ -1,20 +1,17 @@
 var angkaKaskus = function (n) {
-    var hasil = [];
-    for (var x = 1; x <= n; x++) {
-        var y = x * 3;
-        if (y % 5 === 0 && y % 6 === 0) {
-            hasil.push("KASKUS");
-        }
-        else if (y % 5 === 0) {
-            hasil.push("KAS");
-        }
-        else if (y % 6 === 0) {
-            hasil.push("KUS");
-        }
-        else {
-            hasil.push(y);
-        }
+  var hasil = [];
+  for (var x = 1; x <= n; x++) {
+    var y = x * 3;
+    if (y % 5 === 0 && y % 6 === 0) {
+      hasil.push("KASKUS");
+    } else if (y % 5 === 0) {
+      hasil.push("KAS");
+    } else if (y % 6 === 0) {
+      hasil.push("KUS");
+    } else {
+      hasil.push(y);
     }
-    console.log(hasil);
+  }
+  console.log(hasil);
 };
 angkaKaskus(10);
